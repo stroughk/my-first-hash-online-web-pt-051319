a41815bd778b3c6c8691eb34849eb3961a14f1fa
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-26 13:43:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+ my_hash = {"name" => "Kateryna", "age" => 40 } # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
 end
