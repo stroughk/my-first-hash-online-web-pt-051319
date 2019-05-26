63b07a3387e35bbe7e10ec19854fd52b577f246b
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-26 13:48:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,6 +38,6 @@ shipping_manifest["muskets"] = 2     # add 2 muskets to the shipping_manifest ha
 
 
 
-  # return the shipping_manifest hash below
+  shipping_manifest # return the shipping_manifest hash below
 
 end
